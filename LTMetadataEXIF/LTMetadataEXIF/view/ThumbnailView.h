c49fb99174ce50b15c2215c6063063877257650b
Adam <976660802@qq.com>
2017-03-12 18:35:33 +0800

    create  thumbnailview

---
+++ LTMetadataEXIF/LTMetadataEXIF/view/ThumbnailView.h
@@ -0,0 +1,13 @@
+//
+//  ThumbnailView.h
+//  LTMetadataEXIF
+//
+//  Created by Adam on 17/3/12.
+//  Copyright © 2017 adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThumbnailView : UIView
+
+@end
