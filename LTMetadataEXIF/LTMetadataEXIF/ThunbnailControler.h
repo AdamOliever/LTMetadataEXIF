22f96d5b0648694715d4105d0fa29f8c558dcdf5
Adam <976660802@qq.com>
2017-03-19 20:16:56 +0800

    ThumbnailControler Create

---
+++ LTMetadataEXIF/LTMetadataEXIF/ThunbnailControler.h
@@ -0,0 +1,13 @@
+//
+//  ThunbnailControler.h
+//  LTMetadataEXIF
+//
+//  Created by Adam on 17/3/19.
+//  Copyright © 2017年 苗盼盼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ThunbnailControler : ViewController<UINavigationBarDelegate,UIImagePickerControllerDelegate>
+
+@end
---
+++ LTMetadataEXIF/LTMetadataEXIF/ThunbnailControler.m
@@ -0,0 +1,74 @@
+//
+//  ThunbnailControler.m
+//  LTMetadataEXIF
+//
+//  Created by Adam on 17/3/19.
+//  Copyright © 2017年 苗盼盼. All rights reserved.
+//
+
+#import "ThunbnailControler.h"
+#import <Photos/Photos.h>
+@interface ThunbnailControler ()
+
+@end
+
+@implementation ThunbnailControler
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self getOriginalImages];
+    // Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)getOriginalImages
+{
+    // 获得所有的自定义相簿
+    PHFetchResult<PHAssetCollection *> *assetCollections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    // 遍历所有的自定义相簿
+    for (PHAssetCollection *assetCollection in assetCollections) {
+        [self enumerateAssetsInAssetCollection:assetCollection original:YES];
+    }
+    
+    // 获得相机胶卷
+    PHAssetCollection *cameraRoll = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil].lastObject;
+    // 遍历相机胶卷,获取大图
+    [self enumerateAssetsInAssetCollection:cameraRoll original:YES];
+}
+
+- (void)enumerateAssetsInAssetCollection:(PHAssetCollection *)assetCollection original:(BOOL)original
+{
+    NSLog(@"相簿名:%@", assetCollection.localizedTitle);
+    
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    // 同步获得图片, 只会返回1张图片
+    options.synchronous = YES;
+    
+    // 获得某个相簿中的所有PHAsset对象
+    PHFetchResult<PHAsset *> *assets = [PHAsset fetchAssetsInAssetCollection:assetCollection options:nil];
+    for (PHAsset *asset in assets) {
+        // 是否要原图
+        CGSize size = original ? CGSizeMake(asset.pixelWidth, asset.pixelHeight) : CGSizeZero;
+        
+        // 从asset中获得图片
+        [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:size contentMode:PHImageContentModeDefault options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+            NSLog(@"%@", result);
+        }];
+    }
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
